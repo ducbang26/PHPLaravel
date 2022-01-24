5c9657608c89b25331c330873dae8704a45d207c
ducbang_26 <0306191396@caothang.edu.vn>
2022-01-24 16:54:52 +0700

    seeder date create post

--- shop/database/seeders/PostSeeder.php
+++ shop/database/seeders/PostSeeder.php
@@ -20,18 +20,24 @@ class PostSeeder extends Seeder
             'content'=>'Đà Lạt đẹp quá bà con ơi. Tết này đi là hết ý!!',
             'star'=>5,
             'popular'=>true,
+            'updated_at'=>'2022-01-23T09:46:46.000000Z',
+            'created_at'=>'2022-01-23T09:46:46.000000Z'
             ],
             ['place_id'=>2,
             'user_id'=>1,
             'content'=>'Phố cổ Hội An  này tui đi có tầm chục lần mà có chán đâu hehe',
             'star'=>5,
             'popular'=>false,
+            'updated_at'=>'2022-01-22T09:46:46.000000Z',
+            'created_at'=>'2022-01-22T09:46:46.000000Z'
             ],
             ['place_id'=>3,
             'user_id'=>2,
             'content'=>'resort Mũi Né trên cả tuyệt vời á anh em. Có dịp tui sẽ trở lại',
             'star'=>5,
             'popular'=>true,
+            'updated_at'=>'2022-01-24T09:46:46.000000Z',
+            'created_at'=>'2022-01-24T09:46:46.000000Z'
             ],
         ]);
     }
